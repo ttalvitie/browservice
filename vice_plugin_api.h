9f999fe6cb55d66f7925e23235fc71312134b52a
Topi Talvitie <topi.talvitie@iki.fi>
2020-11-21 23:58:02 +0200

    Write general documentation to the plugin API header.

--- vice_plugin_api.h
+++ vice_plugin_api.h
@@ -1,8 +1,42 @@
 #pragma once
 
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
 extern "C" {
+#endif
+
+/**********************************
+ *** Vice Plugin API Definition ***
+ **********************************/
+
+/* This file describes the common API for vice plugins: shared libraries that can be used by an
+ * interactive program to show its GUI to the user.
+ *
+ * The vice plugin API was originally designed as an abstraction of the user-facing part of the
+ * Browservice web "proxy" server. Browservice makes it possible to browse the modern web on
+ * obsolete operating systems and hardware by rendering the browser UI on the server side. The
+ * plugin architecture makes it easy to add support for different types of clients to Browservice
+ * while reusing most of the code.
+ *
+ * The API is not specific to Browservice, and thus the same plugins may be used for other kinds of
+ * GUI programs as well.
+ *
+ * The GUI shown by the plugin consists of multiple windows; the program supplies the plugin with
+ * updates to a resizable 24-bit RGB image view for each window, and the plugin sends the keyboard
+ * and mouse events concerning the windows back to the program. New windows can be initiated both by
+ * the plugin and the program. The program may send files to the user through the plugin. The
+ * program may also allow the user to access its clipboard through the plugin.
+ */
+
+/********************************************
+ *** Functions common to all API versions ***
+ ********************************************/
 
 /* Returns 1 if the plugin supports the given API version; otherwise, returns 0. */
 int vicePluginAPI_isAPIVersionSupported(uint64_t apiVersion);
 
+#ifdef __cplusplus
 }
+#endif
