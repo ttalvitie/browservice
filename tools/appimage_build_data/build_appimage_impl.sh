b41db7a7ca70c7e49330c548727fb6f59c8e7af7
Topi Talvitie <topi.talvitie@iki.fi>
2021-05-22 18:36:30 +0300

    Add copyright information to AppImage.

--- tools/appimage_build_data/build_appimage_impl.sh
+++ tools/appimage_build_data/build_appimage_impl.sh
@@ -103,7 +103,18 @@ do
     U ldd "${f}" | U grep "=>" | U bash -c "awk '{ print \$3 }' >> deplisttmp"
 done
 
-U sort deplisttmp | U grep -v libcef.so | U bash -c "uniq > deplist"
+U sort deplisttmp \
+    | U grep -v libcef.so \
+    | U grep -v libc.so \
+    | U grep -v libdl.so \
+    | U grep -v libgmp.so \
+    | U grep -v libidn2.so \
+    | U grep -v libm.so \
+    | U grep -v libpthread.so \
+    | U grep -v libresolv.so \
+    | U grep -v librt.so \
+    | U grep -v libstdc++.so \
+    | U bash -c "uniq > deplist"
 U rm deplisttmp
 U mkdir deps
 for f in $(cat deplist)
@@ -111,28 +122,14 @@ do
     U cp "${f}" deps
 done
 
-U bash -c "echo \$(ls deps | sort) > deplist"
-msg "All library dependencies: $(cat deplist)"
+U bash -c "echo \$(ls deps | sort) > depnamelist"
+msg "Library dependencies: $(cat depnamelist)"
 
-if [ "$(cat deplist)" != "libGL.so.1 libGLX.so.0 libGLdispatch.so.0 libPocoCrypto.so.50 libPocoFoundation.so.50 libPocoNet.so.50 libX11.so.6 libXau.so.6 libXcomposite.so.1 libXcursor.so.1 libXdamage.so.1 libXdmcp.so.6 libXext.so.6 libXfixes.so.3 libXfont2.so.2 libXi.so.6 libXinerama.so.1 libXmuu.so.1 libXrandr.so.2 libXrender.so.1 libasound.so.2 libatk-1.0.so.0 libatk-bridge-2.0.so.0 libatspi.so.0 libaudit.so.1 libavahi-client.so.3 libavahi-common.so.3 libblkid.so.1 libbsd.so.0 libbz2.so.1.0 libc.so.6 libcairo-gobject.so.2 libcairo.so.2 libcap-ng.so.0 libcom_err.so.2 libcrypto.so.1.1 libcups.so.2 libdatrie.so.1 libdbus-1.so.3 libdl.so.2 libdrm.so.2 libepoxy.so.0 libexpat.so.1 libffi.so.6 libfontconfig.so.1 libfontenc.so.1 libfreetype.so.6 libgbm.so.1 libgcc_s.so.1 libgcrypt.so.20 libgdk-3.so.0 libgdk_pixbuf-2.0.so.0 libgio-2.0.so.0 libglib-2.0.so.0 libgmodule-2.0.so.0 libgmp.so.10 libgnutls.so.30 libgobject-2.0.so.0 libgpg-error.so.0 libgraphite2.so.3 libgssapi_krb5.so.2 libgtk-3.so.0 libharfbuzz.so.0 libhogweed.so.4 libidn2.so.0 libjpeg.so.8 libk5crypto.so.3 libkeyutils.so.1 libkrb5.so.3 libkrb5support.so.0 liblz4.so.1 liblzma.so.5 libm.so.6 libmount.so.1 libnettle.so.6 libnspr4.so libnss3.so libnssutil3.so libp11-kit.so.0 libpango-1.0.so.0 libpangocairo-1.0.so.0 libpangoft2-1.0.so.0 libpcre.so.3 libpixman-1.so.0 libplc4.so libplds4.so libpng16.so.16 libpthread.so.0 libresolv.so.2 librt.so.1 libselinux.so.1 libsmime3.so libsqlite3.so.0 libssl.so.1.1 libstdc++.so.6 libsystemd.so.0 libtasn1.so.6 libthai.so.0 libunistring.so.2 libuuid.so.1 libwayland-client.so.0 libwayland-cursor.so.0 libwayland-egl.so.1 libwayland-server.so.0 libxcb-render.so.0 libxcb-shm.so.0 libxcb.so.1 libxkbcommon.so.0 libxshmfence.so.1 libz.so.1" ]
+if [ "$(cat depnamelist)" != "libGL.so.1 libGLX.so.0 libGLdispatch.so.0 libPocoCrypto.so.50 libPocoFoundation.so.50 libPocoNet.so.50 libX11.so.6 libXau.so.6 libXcomposite.so.1 libXcursor.so.1 libXdamage.so.1 libXdmcp.so.6 libXext.so.6 libXfixes.so.3 libXfont2.so.2 libXi.so.6 libXinerama.so.1 libXmuu.so.1 libXrandr.so.2 libXrender.so.1 libasound.so.2 libatk-1.0.so.0 libatk-bridge-2.0.so.0 libatspi.so.0 libaudit.so.1 libavahi-client.so.3 libavahi-common.so.3 libblkid.so.1 libbsd.so.0 libbz2.so.1.0 libcairo-gobject.so.2 libcairo.so.2 libcap-ng.so.0 libcom_err.so.2 libcrypto.so.1.1 libcups.so.2 libdatrie.so.1 libdbus-1.so.3 libdrm.so.2 libepoxy.so.0 libexpat.so.1 libffi.so.6 libfontconfig.so.1 libfontenc.so.1 libfreetype.so.6 libgbm.so.1 libgcc_s.so.1 libgcrypt.so.20 libgdk-3.so.0 libgdk_pixbuf-2.0.so.0 libgio-2.0.so.0 libglib-2.0.so.0 libgmodule-2.0.so.0 libgnutls.so.30 libgobject-2.0.so.0 libgpg-error.so.0 libgraphite2.so.3 libgssapi_krb5.so.2 libgtk-3.so.0 libharfbuzz.so.0 libhogweed.so.4 libjpeg.so.8 libk5crypto.so.3 libkeyutils.so.1 libkrb5.so.3 libkrb5support.so.0 liblz4.so.1 liblzma.so.5 libmount.so.1 libnettle.so.6 libnspr4.so libnss3.so libnssutil3.so libp11-kit.so.0 libpango-1.0.so.0 libpangocairo-1.0.so.0 libpangoft2-1.0.so.0 libpcre.so.3 libpixman-1.so.0 libplc4.so libplds4.so libpng16.so.16 libselinux.so.1 libsmime3.so libsqlite3.so.0 libssl.so.1.1 libsystemd.so.0 libtasn1.so.6 libthai.so.0 libunistring.so.2 libuuid.so.1 libwayland-client.so.0 libwayland-cursor.so.0 libwayland-egl.so.1 libwayland-server.so.0 libxcb-render.so.0 libxcb-shm.so.0 libxcb.so.1 libxkbcommon.so.0 libxshmfence.so.1 libz.so.1" ]
 then
     msg "Error: Unexpected list of library dependencies; build_appimage_impl.sh must be updated"
     false
 fi
-
-msg "Filtering library dependencies"
-U rm deps/libc.so.*
-U rm deps/libdl.so.*
-U rm deps/libgmp.so.1*
-U rm deps/libidn2.so.*
-U rm deps/libm.so.*
-U rm deps/libpthread.so.*
-U rm deps/libresolv.so.*
-U rm deps/librt.so.*
-U rm deps/libstdc++.so.*
-
-U bash -c "echo \$(ls deps | sort) > deplist"
-msg "Filtered library dependencies: $(cat deplist)"
 
 msg "Adding run-time library dependencies"
 U cp -r "${NSSDIR}" deps/nss
@@ -192,11 +189,10 @@ U patchelf --set-rpath '$ORIGIN/../../lib' bin/hack/Xvfb
 U patchelf --set-rpath '$ORIGIN/../lib' bin/xauth
 U patchelf --set-rpath '$ORIGIN/../lib' bin/relocate_fontconfig_cache
 
-msg "Recording font copyright information"
+msg "Recording dependency and font copyright information"
 U mkdir doc
-for pkg in $((dpkg -S $(find /usr/share/fonts/ -type f) 2> /dev/null || true) | awk '{ print $1 }' | sort | uniq)
+for pkg in $((dpkg -S /usr/bin/Xvfb /usr/bin/xauth $(cat deplist) $(find /usr/share/fonts/ -type f) $(find "${NSSDIR}" -type f) 2> /dev/null || true) | sed 's/:/ /g' | awk '{ print $1 }' | sort | uniq)
 do
-    pkg="${pkg%:}"
     U mkdir "doc/${pkg}"
     U cp "/usr/share/doc/${pkg}/copyright" "doc/${pkg}/copyright"
 done
