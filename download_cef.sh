005aea5e9b79629797c99e614d07003aa278862a
Topi Talvitie <topi.talvitie@iki.fi>
2021-05-27 22:50:16 +0300

    Upgrade to CEF 90.6.7.

--- download_cef.sh
+++ download_cef.sh
@@ -12,20 +12,20 @@ ARCH=`uname -m`
 
 if [ "$ARCH" == "x86_64" ]
 then
-    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.6%2Bg3c44b04%2Bchromium-90.0.4430.93_linux64.tar.bz2" -O cef_unverified.tar.bz2
-    SHA1="bddafa459df7a6b865b2066ac16c8e0f82b1cab9"
+    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.7%2Bg19ba721%2Bchromium-90.0.4430.212_linux64.tar.bz2" -O cef_unverified.tar.bz2
+    SHA1="f449aac4e0ce2d0a3f6da0005a0ade010b83103c"
 elif [[ "$ARCH" =~ ^i[3-7]86$ ]]
 then
-    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.6%2Bg3c44b04%2Bchromium-90.0.4430.93_linux32.tar.bz2" -O cef_unverified.tar.bz2
-    SHA1="c43df8549b701ffef7e3635ddab535e3eb696fd7"
+    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.7%2Bg19ba721%2Bchromium-90.0.4430.212_linux32.tar.bz2" -O cef_unverified.tar.bz2
+    SHA1="7d90fed73ac42020973363ad924fd6fbad7a524b"
 elif [ "$ARCH" == "armv7l" ]
 then
-    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.6%2Bg3c44b04%2Bchromium-90.0.4430.93_linuxarm.tar.bz2" -O cef_unverified.tar.bz2
-    SHA1="c9357f85c34c0daaa9cb731a251f45510fcf124b"
+    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.7%2Bg19ba721%2Bchromium-90.0.4430.212_linuxarm.tar.bz2" -O cef_unverified.tar.bz2
+    SHA1="4c00009546821772ea6fe9a96b99f3843a9edf30"
 elif [ "$ARCH" == "aarch64" ]
 then
-    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.6%2Bg3c44b04%2Bchromium-90.0.4430.93_linuxarm64.tar.bz2" -O cef_unverified.tar.bz2
-    SHA1="2fb0ee0302f2c1e2304465def26d53d721b60db7"
+    wget "https://cef-builds.spotifycdn.com/cef_binary_90.6.7%2Bg19ba721%2Bchromium-90.0.4430.212_linuxarm64.tar.bz2" -O cef_unverified.tar.bz2
+    SHA1="828c1aad32eecfecccc120e1f0cebf629891eb05"
 else
     echo "Unsupported architecture"
     exit 1
