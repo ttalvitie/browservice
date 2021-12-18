273355666d00ecee933f604e8bb67df25abc70e7
Topi Talvitie <topi.talvitie@iki.fi>
2021-12-18 15:49:18 +0200

    Include OpenSSL copyright information.

--- viceplugins/retrojsvice/src/credits.cpp
+++ viceplugins/retrojsvice/src/credits.cpp
@@ -13,6 +13,11 @@ the IJG (Independent JPEG Group) License, the 3-clause BSD license and the zlib 
 Retrojsvice uses zlib
 (c) 1995-2017 Jean-loup Gailly and Mark Adler
 Licensed under the zlib License.
+
+Retrojsvice uses OpenSSL
+(c) 1998-2019 The OpenSSL Project
+(c) 1995-1998 Eric Young
+which is licensed under the OpenSSL License and the original SSLeay license.
 )DELIM";
 
 }
