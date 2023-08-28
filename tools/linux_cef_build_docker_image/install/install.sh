3c48104ab8bb9dfd441da6fd1904d859685a77d8
Topi Talvitie <topi.talvitie@iki.fi>
2023-08-28 10:13:26 +0300

    Update Linux Docker image build script.

--- tools/linux_cef_build_docker_image/install/install.sh
+++ tools/linux_cef_build_docker_image/install/install.sh
@@ -6,7 +6,8 @@ chromium_version=116.0.5845.111
 # https://magpcss.org/ceforum/viewtopic.php?f=7&t=17776&p=46448#p47371
 # linked from official automated build setup documentation in
 # https://bitbucket.org/chromiumembedded/cef/wiki/AutomatedBuildSetup.md
-# (Fixed python -> python3 in the apt-get install command below.)
+# (Fixed python -> python3 in the apt-get install command and updated the
+# bootstrap download and "Installing locales" fix to use the Python script.)
 
 # Packages required to download and run the Chromium bootstrap.
 apt-get update
@@ -17,13 +18,12 @@ apt-get install -y \
 
 # Download the Chromium bootstrap at the requested version.
 curl "https://chromium.googlesource.com/chromium/src/+/refs/tags/${chromium_version}/build/install-build-deps.sh?format=TEXT" | base64 -d > install-build-deps.sh
+curl "https://chromium.googlesource.com/chromium/src/+/refs/tags/${chromium_version}/build/install-build-deps.py?format=TEXT" | base64 -d > install-build-deps.py
 chmod 755 install-build-deps.sh
+chmod 755 install-build-deps.py
 
 # Remove the "Installing locales" step which fails with sed errors.
-# TODO: This trims all the way to end-of-file. Fix the sed error or
-# add a more nuanced approach in the future if necessary.
-line_num="$(grep -n 'echo \"Installing locales.\"' install-build-deps.sh | head -n 1 | cut -d: -f1)"
-sed -i "${line_num},\$ d" install-build-deps.sh
+sed -i 's/print(\"Installing locales.\", file=sys.stderr)/return/' install-build-deps.py
 
 # The install script expects sudo, so make a placeholder that transparently forwards commands.
 if type sudo 2>/dev/null; then
