8ca81dc1a2022ca6e9a7ca332092b6523536f234
Topi Talvitie <topi.talvitie@iki.fi>
2023-11-04 12:25:20 +0200

    Switch to Fedora 38 for smoke test as the cloud images for 36 are no longer available.

---
+++ tools/qemu_smoke_test_data/config_fedora_38_aarch64.sh
@@ -0,0 +1,4 @@
+ARCH="aarch64"
+RUNNER="fedora"
+USE_AARCH64_EFI="yes"
+IMAGE_URL="https://download.fedoraproject.org/pub/fedora/linux/releases/38/Cloud/aarch64/images/Fedora-Cloud-Base-38-1.6.aarch64.qcow2"
