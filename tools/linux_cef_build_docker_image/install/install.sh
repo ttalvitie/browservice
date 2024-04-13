ad7a1227ea2bf9edd0e0561e01737774472e9cc2
Topi Talvitie <topi.talvitie@iki.fi>
2024-04-13 20:57:24 +0300

    Update CEF Docker build image.

--- tools/linux_cef_build_docker_image/install/install.sh
+++ tools/linux_cef_build_docker_image/install/install.sh
@@ -3,29 +3,20 @@
 chromium_version=123.0.6312.107
 
 # Script from
-# https://magpcss.org/ceforum/viewtopic.php?f=7&t=17776&p=46448#p47371
+# https://magpcss.org/ceforum/viewtopic.php?f=7&t=17776&p=46448#p54107
 # linked from official automated build setup documentation in
 # https://bitbucket.org/chromiumembedded/cef/wiki/AutomatedBuildSetup.md
-# (Fixed python -> python3 and added file in the apt-get install command and
-# updated the bootstrap download and "Installing locales" fix to use the
-# Python script.)
 
 # Packages required to download and run the Chromium bootstrap.
 apt-get update
 apt-get install -y \
     curl \
+    file \
     lsb-release \
-    python3 \
-    file
+    python3
 
 # Download the Chromium bootstrap at the requested version.
-curl "https://chromium.googlesource.com/chromium/src/+/refs/tags/${chromium_version}/build/install-build-deps.sh?format=TEXT" | base64 -d > install-build-deps.sh
 curl "https://chromium.googlesource.com/chromium/src/+/refs/tags/${chromium_version}/build/install-build-deps.py?format=TEXT" | base64 -d > install-build-deps.py
-chmod 755 install-build-deps.sh
-chmod 755 install-build-deps.py
-
-# Remove the "Installing locales" step which fails with sed errors.
-sed -i 's/print(\"Installing locales.\", file=sys.stderr)/return/' install-build-deps.py
 
 # The install script expects sudo, so make a placeholder that transparently forwards commands.
 if type sudo 2>/dev/null; then
@@ -35,12 +26,21 @@ else
  chmod +x /usr/sbin/sudo;
 fi
 
-# Install with i386 and ARM dependencies included.
-./install-build-deps.sh --lib32 --arm --no-chromeos-fonts --no-nacl --no-prompt
+# Install with 32-bit and ARM dependencies included.
+# NOTE: Installing locales fails, but we ignore the error.
+python3 ./install-build-deps.py --lib32 --arm --no-chromeos-fonts --no-nacl --no-prompt
 
-# Fix the following error with i386 and ARM builds:
-# ././v8_context_snapshot_generator: error while loading shared libraries: libglib-2.0.so.0: cannot open shared object file: No such file or directory
-apt-get install -y libglib2.0-0:i386
+# Install pgrep which is used by depot_tools/.cipd_bin/goma_ctl.py.
+apt-get install -y procps
 
-# Cleanup packages to reduce image size.
+# Install python dependencies required by Chromium.
+apt-get install -y python3-pip
+python3 -m pip install \
+    dataclasses \
+    importlib_metadata
+
+# Install Doxygen which is used to generate CEF API documentation.
+apt-get install -y doxygen
+
+# Cleanup to reduce image size.
 apt-get purge -y --auto-remove
