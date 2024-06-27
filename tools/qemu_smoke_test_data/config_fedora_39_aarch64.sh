f213a10fb884294f090fd05100c3c7e7fe27675c
Topi Talvitie <topi.talvitie@iki.fi>
2024-06-27 12:58:05 +0300

    Remove Fedora 39 aarch64 smoke test as it is unable to boot.

--- tools/qemu_smoke_test_data/config_fedora_39_aarch64.sh
+++
@@ -1,4 +0,0 @@
-ARCH="aarch64"
-RUNNER="fedora"
-USE_AARCH64_EFI="yes"
-IMAGE_URL="https://download.fedoraproject.org/pub/fedora/linux/releases/39/Cloud/aarch64/images/Fedora-Cloud-Base-39-1.5.aarch64.qcow2"
