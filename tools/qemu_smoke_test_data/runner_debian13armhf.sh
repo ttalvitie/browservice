2ea36613d2dbebf5e7af1a42c9b59d4aca5c6915
Topi Talvitie <topi.talvitie@iki.fi>
2026-02-05 19:27:01 +0200

    Make raspi armhf smoke test succeed by using extracted AppImage.

---
+++ tools/qemu_smoke_test_data/runner_debian13armhf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+shopt -s expand_aliases
+
+msg() { cat <<< "--- $@" 1>&2; }
+
+msg "Creating normal user for running the tests"
+useradd -m user
+alias U="sudo -u user"
+
+msg "Copying AppImage and smoke test data to home directory"
+cd /home/user
+U cp /shared/browservice.AppImage browservice.AppImage
+U cp /shared/smoke_test.py smoke_test.py
+U cp -r /shared/smoke_test_data smoke_test_data
+U chmod 700 browservice.AppImage
+
+msg "Disabling man-db update to save time"
+rm /var/lib/man-db/auto-update
+
+msg "Disabling initramfs update to save time and avoid issues"
+echo "update_initramfs=no" >> /etc/initramfs-tools/update-initramfs.conf
+
+msg "Installing dependencies"
+export DEBIAN_FRONTEND=noninteractive
+echo 'Acquire::Retries "5";' > /etc/apt/apt.conf.d/80-retries
+apt-get update
+apt-get install -y python3-pip python3-numpy libjpeg-dev zlib1g-dev libopenjp2-7
+U pip install --break-system-packages imageio
+
+msg "Extracting the AppImage"
+U ./browservice.AppImage --appimage-extract
+
+msg "Running smoke test"
+U python3 smoke_test.py ./squashfs-root/AppRun all
